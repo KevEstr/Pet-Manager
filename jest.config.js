ff4d6be6febc56ae5b4935f920e88bbaff19e1ae
apoyodesarrollo.ingenieria6 <apoyodesarrollo6.ing@udea.edu.co>
2025-06-12 21:24:02 -0500

    Unitarias

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,19 @@ const customConfig = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['lcov', 'text'],
+
+  // Indicar qué archivos incluir/excluir en el informe de cobertura
+  collectCoverageFrom: [
+    'pages/**/*.{js,jsx,ts,tsx}',
+    'components/**/*.{js,jsx,ts,tsx}',
+    'lib/**/*.{js,jsx,ts,tsx}',
+    // Excluir archivos de configuración de Next.js y definiciones de tipo
+    '!pages/_app.{js,jsx,ts,tsx}',
+    '!pages/_document.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/*.d.ts'
+  ],
+
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js|jsx)',
